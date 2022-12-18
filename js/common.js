0ffefd0a23469f7e54197d256c214cd601a05a63
AsukaTanaka <111677080+AsukaTanaka@users.noreply.github.com>
2022-12-19 00:12:14 +0700

    Update

--- js/common.js
+++
@@ -1,3 +0,0 @@
-const sectionIndex = document.querySelector('.section-index');
-const sectionNews = document.querySelector('.section-news');
-const siteSTContent = document.querySelector(".site-st-content");
