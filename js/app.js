6201c90cc29f62a2c3dee3f539661702f17fc1c3
AsukaTanaka <111677080+AsukaTanaka@users.noreply.github.com>
2022-12-19 20:00:51 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 function init() {
-    
+    let page = document.querySelector(".site-section").id;
+
+    switch (page) {
+        case "post-index" :
+            
+            break;
+
+        default :
+            break;
+    }
 }
 
 init();
